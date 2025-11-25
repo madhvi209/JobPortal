eec0ba9e8b77a1b3c7be1fdd11252bcd1d8a1095
madhvi209 <madhavisharma209@gmail.com>
2025-11-25 19:49:22 +0530

    update

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -5,14 +5,14 @@ import { Link } from 'react-router-dom';
 const LatestJobs = () => {
     const { allJobs } = useSelector(store => store.job);
     return (
-        <div className='max-w-7xl mx-auto my-20'>
+        <div className='max-w-7xl mx-auto my-20 ml-4'>
             <h1 className='text-4xl font-bold'>
                 <span className='text-[#6A38C2]'>Latest and Top</span> Job Openings
             </h1>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-5'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 my-5 place-items-center'>
                 {
                     allJobs && allJobs?.slice(0, 6).map(job => (
-                        <Link key={job._id} to={`/description/${job?._id}`}>
+                        <Link key={job._id} to={`/description/${job?._id}`} className="flex justify-center w-full">
                             <LatestJobCard job={job} />
                         </Link>
                     ))
